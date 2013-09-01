712a758bb53c978bb5034ed8053d1ba92fadd422
Chris Wessels <undefined.za@gmail.com>
2013-09-01 15:14:38 +0200

    Fixed package.js.

--- package.js
+++ package.js
@@ -3,13 +3,9 @@ Package.describe({
 });
 
 Package.on_use(function (api){
-	api.add_files('lib/font-awesome/font/fontawesome-webfont.eot', 'client');
-	api.add_files('lib/font-awesome/font/fontawesome-webfont.svg', 'client');
-	api.add_files('lib/font-awesome/font/fontawesome-webfont.ttf', 'client');
-	api.add_files('lib/font-awesome/font/fontawesome-webfont.woff', 'client');
-	api.add_files('lib/font-awesome/font/FontAwesome.otf', 'client');
-	api.add_files('lib/font-awesome/css/font-awesome.css', 'client');
-
-	//font path override
-	api.add_files('path-override.css', 'client');
+  api.add_files('lib/glyphicons-halflings/fonts/glyphicons-halflings-regular.eot', 'client');
+  api.add_files('lib/glyphicons-halflings/fonts/glyphicons-halflings-regular.svg', 'client');
+  api.add_files('lib/glyphicons-halflings/fonts/glyphicons-halflings-regular.ttf', 'client');
+  api.add_files('lib/glyphicons-halflings/fonts/glyphicons-halflings-regular.woff', 'client');
+	api.add_files('lib/glyphicons-halflings/css/glyphicons.css', 'client');
 });
