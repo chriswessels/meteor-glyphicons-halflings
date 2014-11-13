48695dd0fc135fad84acfbcb92237890739ef4c3
Chris Wessels <undefined.za@gmail.com>
2014-11-13 18:53:41 +0000

    Update to 1.0 package conventions.

--- package.js
+++ package.js
@@ -1,11 +1,14 @@
 Package.describe({
-	summary: "This simple smart package adds the Glyphicon Halflings font-face to Meteor."
+  name: 'chriswessels:glyphicons-halflings',
+  version: '2.0.0',
+  summary: "This simple smart package adds the Glyphicon Halflings font-face to Meteor.",
+  git: 'https://github.com/chriswessels/meteor-glyphicons-halflings.git'
 });
 
-Package.on_use(function (api){
-  api.add_files('lib/glyphicons-halflings/fonts/glyphicons-halflings-regular.eot', 'client');
-  api.add_files('lib/glyphicons-halflings/fonts/glyphicons-halflings-regular.svg', 'client');
-  api.add_files('lib/glyphicons-halflings/fonts/glyphicons-halflings-regular.ttf', 'client');
-  api.add_files('lib/glyphicons-halflings/fonts/glyphicons-halflings-regular.woff', 'client');
-	api.add_files('lib/glyphicons-halflings/css/glyphicons.css', 'client');
+Package.onUse(function (api){
+  api.addFiles('lib/glyphicons-halflings/fonts/glyphicons-halflings-regular.eot', 'client');
+  api.addFiles('lib/glyphicons-halflings/fonts/glyphicons-halflings-regular.svg', 'client');
+  api.addFiles('lib/glyphicons-halflings/fonts/glyphicons-halflings-regular.ttf', 'client');
+  api.addFiles('lib/glyphicons-halflings/fonts/glyphicons-halflings-regular.woff', 'client');
+  api.addFiles('lib/glyphicons-halflings/css/glyphicons.css', 'client');
 });
